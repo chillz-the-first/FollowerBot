9371db65ab67b2991f10a9c6e0c7978f973c0b55
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-08-21 12:47:10 +0200

    Completed the login method

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 class InstaFollower:
     def __init__(self):
-        self.username = os.environ.get("USERNAME")
+        self.username = os.environ.get("IG_USERNAME")
         self.password = os.environ.get("PASSWORD")
         self.similar_acc = os.environ.get("SIMILAR_ACCOUNT")
         self.url = os.environ.get("BASE_URL")
@@ -21,7 +21,26 @@ class InstaFollower:
         self.wait = WebDriverWait(self.driver, 10)
 
     def login(self):
-        pass
+        print("Logging in...")
+        self.driver.get(self.login_url)
+
+        username_input = self.wait.until(EC.element_to_be_clickable((By.ID, "username")))
+        username_input.send_keys(self.username)
+
+        password_input = self.wait.until(EC.element_to_be_clickable((By.ID, "password")))
+        password_input.send_keys(self.password)
+
+        login_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Log in']")))
+        login_btn.click()
+
+        # self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popup-save-login"]/div/div[2]'))).click()
+        # self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popup-notifications"]/div/button[2]'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Not now')]"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Not Now']"))).click()
+
+        # self.wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/nav/a[1]')))
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "naan-rail-logo")))
+        print("Logged in...")
 
     def find_followers(self):
         pass
@@ -37,4 +56,5 @@ try:
 except TimeoutException:
     print("A step failed, aborting run")
 finally:
-    bot.driver.quit()
+    if bot is not None:
+        bot.driver.quit()
